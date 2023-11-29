11d1ee40dea9bc29d9721ea872f3bd27cb633d9c
sneha-rohini <sneharohini2001@gmail.com>
2023-11-29 21:08:04 +0530

    fgtrff

---
+++ ecommerceproject/shopapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+app_name='shopapp'
+urlpatterns = [
+    path('',views.allProCat,name='allProCat'),
+    path('<slug:c_slug>/',views.allProCat,name='products_by_category'),
+    path('<slug:c_slug>/<slug:product_slug>/',views.proDetail,name='proCatdetail'),
+
+]
